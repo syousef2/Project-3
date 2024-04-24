f4035815ac1f751c51a84f534c9a22a417edc5c8
syousef2 <syousef@hawk.iit.edu>
2024-04-23 20:46:44 -0500

    adding my own features

--- js/map.js
+++ js/map.js
@@ -1,23 +1,23 @@
 function init(){
-	//alert('it works');
 	var el = document.getElementById('canvas');
 	var myLocation = new google.maps.LatLng(41.835117, -87.627130);
 	var mapOptions = {
 		center: myLocation,
 		zoom: 18,
 		mapTypeId: google.maps.MapTypeId.SATELLITE,
+    mapTypeControl: true,
 		mapTypeControlOptions: {
 			position: google.maps.ControlPosition.BOTTOM_CENTER
 		}
 	};
 
 	var myMap = new google.maps.Map(el, mapOptions);
-
-	var marker = new google.maps.Marker({
+  
+  var marker = new google.maps.Marker({
 		position: myLocation,
 		map: myMap,
-		animation: google.maps.Animation.BOUNCE,
-		icon: './images/iit-icon.png'
+    title: 'IIT Perlstein Hall',
+		icon: './images/mapicon.jpg'
 	});
 
 	var contentString = '<h1>IIT Perlstein Hall</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate odit optio, voluptatem placeat odio dignissimos illo magnam esse asperiores voluptas at iure vero eum, nemo aperiam? Ipsam, atque nobis rem.</p>';
@@ -26,10 +26,24 @@ function init(){
       content: contentString
   	});
 
-	google.maps.event.addListener(marker, 'mouseover', function() {
-    	infowindow.open(myMap, marker);
-  	});
+  var streetViewService = new google.maps.StreetViewService();
+  var streetViewPanorama = myMap.getStreetView();
+  streetViewPanorama.setPosition(myLocation);
+  streetViewPanorama.setPov([
+  heading: 256,
+  pitch; 0
+  });
+ 
+   var searchBox = new google.maps.places.SearchBox(document.getElementById('search-box'));
+    myMap.controls[google.maps.ControlPosition.TOP_LEFT].push(document.getElementById('search-box'));
 
+	google.maps.event.addListener(searchbox, 'places_changed', function() {
+   var places = searchBox.getPlaces();
+   if (places.length == 0) {
+       return;
+
+  	});
+    drawingManager.setMap(myMap);
 
 }
 
